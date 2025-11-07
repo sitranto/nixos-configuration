a441b13f04daad0c7ef83f22a1daafc21f1e242a
sitranto <anatoliyl2006@gmail.com>
2025-11-07 23:01:03 +0300

    fs remake

--- flake.nix
+++ flake.nix
@@ -3,9 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations.sitranto-machine = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs };
       modules = [
